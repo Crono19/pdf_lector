d86fd205b36cafc0806622e1b44f0ff6f6d8dcac
Estuardo Vasquez <vasquezalvarado@hotmail.es>
2023-10-02 22:30:22 -0600

    ventanas funcionales

--- metadata_reader/src/main/java/com/example/metadata_reader/HelloController.java
+++ metadata_reader/src/main/java/com/example/metadata_reader/HelloController.java
@@ -1,14 +1,40 @@
 package com.example.metadata_reader;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToUploadedFilesWindow(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("uploaded_files_window.fxml")));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
     }
+
+    public void switchToLoadFilesWindow(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("load_file_window.fxml")));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
 }
