894fbfacba61f67a926bb1dceac666378f03c9c4
Pablo Mazariegos <mazariegospablo928@gmail.com>
2023-10-11 22:22:24 -0600

    Update HelloApplication

--- metadata_reader/src/main/java/com/example/metadata_reader/HelloApplication.java
+++ metadata_reader/src/main/java/com/example/metadata_reader/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 450, 400);
-        stage.setTitle("Metadata PDF Reader");
+        stage.setTitle("PDF Reader");
         stage.setScene(scene);
         stage.show();
     }
